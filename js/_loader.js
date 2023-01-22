3a3706e6c0405a9f5e13cef4b0651b665c89f1fb
Ronik22 <ronikbhattacharjee22112000@gmail.com>
2023-01-22 22:47:58 +0530

    vendor js files added

--- js/_loader.js
+++
@@ -1,9 +0,0 @@
-const loader = document.querySelector(".spinner-body");
-const body = document.getElementsByTagName("body")[0];
-
-window.addEventListener("load", function () {
-	setTimeout(function () {
-		loader.style.display = "none";
-		body.style.overflow = "auto";
-	}, 1000);
-});
